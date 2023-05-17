7c851dfb0ac2dd5685d6136d9885ffbe135e4351
vanshita shah <vanshita.s@simformsolutions.com>
2023-05-17 14:54:55 +0530

    Added form validations using formik

--- src/components/Signup/Signup.jsx
+++ src/components/Signup/Signup.jsx
@@ -1,7 +1,51 @@
+import { ErrorMessage, Field, Form, Formik } from "formik";
 import React from "react";
-import { Link } from "react-router-dom";
+import ErrorMsg from "../ErrorMsg/ErrorMsg";
 import img from "../../assets/signup.png";
 import Styles from "./Signup.module.css";
+
+const initialValues = {
+  name: "",
+  email: "",
+  profile: "",
+  phone: "",
+  password: "",
+  confirmPassword: "",
+};
+
+const onSubmit = (values, onSubmitprops) => {
+  console.log("submitted");
+  console.log("Form Data", values);
+  onSubmitprops.resetForm();
+};
+
+const validate = (values) => {
+  let errors = {};
+  if (!values.name) {
+    errors.name = "Required!";
+  }
+  if (!values.email) {
+    errors.email = "Required!";
+  } else if (
+    !/^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/.test(values.email)
+  ) {
+    errors.email = "Invaild email format!";
+  }
+  if (!values.phone) {
+    errors.phone = "Required!";
+  } else if (!/^(\+91|0)?[6789]\d{9}$/.test(values.phone)) {
+    errors.phone = "Invalid phone number!";
+  }
+  if (!values.password) {
+    errors.password = "Required!";
+  }
+  if (!values.confirmPassword) {
+    errors.confirmPassword = "Required!";
+  } else if (values.password !== values.confirmPassword) {
+    errors.confirmPassword = "Password and Confirm password does not match!";
+  }
+  return errors;
+};
 
 const Signup = () => {
   return (
@@ -11,41 +55,58 @@ const Signup = () => {
           <div className={Styles.formContainer}>
             <h1 id={Styles.heading}>SignUp</h1>
 
-            <form>
-              <div className={`${Styles.profile} ${Styles.formControl}`}>
-                <label htmlFor="name">Photo +</label>
-              </div>
+            <Formik
+              initialValues={initialValues}
+              validate={validate}
+              onSubmit={onSubmit}
+            >
+              <Form>
+                <div className={`${Styles.profile} ${Styles.formControl}`}>
+                  <label htmlFor="profile">Photo +</label>
+                  <Field
+                    type="file"
+                    name="profile"
+                    id="profile"
+                    accept="Image/jpg,Image/png"
+                    hidden
+                  />
+                  <ErrorMessage name="profile" component={ErrorMsg} />
+                </div>
 
-              <div className={Styles.formControl}>
-                <label htmlFor="name">Name</label>
-                <input type="text" name="name" id="name" />
-              </div>
+                <div className={Styles.formControl}>
+                  <label htmlFor="name">Name</label>
+                  <Field type="text" name="name" id="name" />
+                  <ErrorMessage name="name" component={ErrorMsg} />
+                </div>
 
-              <div className={Styles.formControl}>
-                <label htmlFor="email">Email</label>
-                <input type="Email" name="email" id="email" />
-              </div>
+                <div className={Styles.formControl}>
+                  <label htmlFor="email">Email</label>
+                  <Field type="Email" name="email" id="email" />
+                  <ErrorMessage name="email" component={ErrorMsg} />
+                </div>
 
-              <div className={Styles.formControl}>
-                <label htmlFor="phone">Phone</label>
-                <input type="text" name="phone" id="phone" />
-              </div>
+                <div className={Styles.formControl}>
+                  <label htmlFor="phone">Phone</label>
+                  <Field type="text" name="phone" id="phone" />
+                  <ErrorMessage name="phone" component={ErrorMsg} />
+                </div>
 
-              <div className={Styles.formControl}>
-                <label htmlFor="password">Password</label>
-                <input type="password" name="password" id="password" />
-              </div>
+                <div className={Styles.formControl}>
+                  <label htmlFor="password">Password</label>
+                  <Field type="password" name="password" id="password" />
+                  <ErrorMessage name="password" component={ErrorMsg} />
+                </div>
 
-              <div className={Styles.formControl}>
-                <label htmlFor="confirmPassword">Confirm Password</label>
-                <input
-                  type="password"
-                  name="confirmPassword"
-                  id="confirmPassword"
-                />
-              </div>
+                <div className={Styles.formControl}>
+                  <label htmlFor="confirmPassword">Confirm Password</label>
+                  <Field
+                    type="password"
+                    name="confirmPassword"
+                    id="confirmPassword"
+                  />
+                  <ErrorMessage name="confirmPassword" component={ErrorMsg} />
+                </div>
 
-              <Link to="/home">
                 <button
                   type="submit"
                   className={Styles.btn}
@@ -53,12 +114,16 @@ const Signup = () => {
                 >
                   Signup
                 </button>
-              </Link>
 
-              <button type="reset" className={Styles.btn} id={Styles.resetBtn}>
-                Reset
-              </button>
-            </form>
+                <button
+                  type="reset"
+                  className={Styles.btn}
+                  id={Styles.resetBtn}
+                >
+                  Reset
+                </button>
+              </Form>
+            </Formik>
           </div>
 
           <div className={Styles.imageContainer}>
