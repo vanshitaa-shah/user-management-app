910ce8ec2451c0c8b7609fbe1e2b95944162a332
vanshita shah <vanshita.s@simformsolutions.com>
2023-05-17 13:17:29 +0530

    Configured routing for home and signup

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import React from "react";
+import { useEffect } from "react";
+import { Route, Routes, useNavigate } from "react-router-dom";
 import "./App.css";
 import Home from "./components/Home/Home";
 import Signup from "./components/Signup/Signup";
 
 const App = () => {
+  const navigate = useNavigate();
+  useEffect(() => {
+    navigate("/signUp");
+  }, []);
   return (
-    <>
-      <Signup />
-      <Home />
-    </>
+    <Routes>
+      <Route path="/signup" element={<Signup />}></Route>
+      <Route path="/home" element={<Home />}></Route>
+    </Routes>
   );
 };
 
