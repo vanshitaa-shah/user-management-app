ee46349de247a3ac934359f8c9793c0d611c3443
vanshita shah <vanshita.s@simformsolutions.com>
2023-05-18 15:17:03 +0530

    Squashed commit of branch 'feature/Redux
    commit 21cb3220108295802d4afab25782c6c6a53ab7fd
    Author: vanshita shah <vanshita.s@simformsolutions.com>
    Date:   Thu May 18 14:41:13 2023 +0530
    
        Implemented redux for authentication
    
    commit a51a1c102f30ff8a8cf577c6a3f01dbf439b8d8e
    Author: vanshita shah <vanshita.s@simformsolutions.com>
    Date:   Thu May 18 14:40:23 2023 +0530
    
        Modified style modules
    
    commit b21ecb03a756308ef37c86b01c0536379d8d12a3
    Author: vanshita shah <vanshita.s@simformsolutions.com>
    Date:   Thu May 18 14:39:20 2023 +0530
    
        Added redux and toolkit dependencies
    
    commit 858f58ff75847083ea17731d6ca8a1f825fc1468
    Author: vanshita shah <vanshita.s@simformsolutions.com>
    Date:   Wed May 17 13:17:29 2023 +0530
    
        Merge branch 'feature/Form-Handling' into dev
    
    commit 767a01c05444338053aab867a57de56df9c74b3f
    Author: vanshita shah <vanshita.s@simformsolutions.com>
    Date:   Wed May 17 12:23:14 2023 +0530
    
        Merge branch 'feature/UI' into dev

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
+import store from "./store/index.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
 );
